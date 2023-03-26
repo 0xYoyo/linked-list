f30cd2cec1a3425e25da08f1264b320dcd2d0377
0xYoyo <113042560+0xYoyo@users.noreply.github.com>
2023-03-26 18:48:18 +0300

    add more functionality

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ const linkedList = (() => {
 
   const size = () => {
     if (head == null) {
-      console.log(0);
+      return 0;
     } else {
       let counter = 1;
       let pointer = head;
@@ -40,8 +40,86 @@ const linkedList = (() => {
         counter++;
         pointer = pointer.nextNode;
       }
-      console.log(counter);
+      return counter;
     }
+  };
+
+  const getHead = () => {
+    return head;
+  };
+
+  const getTail = () => {
+    let tail = head;
+    while (tail.nextNode != null) {
+      tail = tail.nextNode;
+    }
+    return tail;
+  };
+
+  const at = (index) => {
+    if (index >= size() || index < 0) {
+      return null;
+    } else {
+      let pointer = head;
+      let counter = 0;
+      while (counter != index) {
+        counter++;
+        pointer = pointer.nextNode;
+      }
+      console.log(pointer);
+      return pointer;
+    }
+  };
+
+  const pop = () => {
+    if (head != null) {
+      if (head.nextNode == null) {
+        head = null;
+      } else {
+        let pointer = head;
+        while (pointer.nextNode.nextNode != null) {
+          pointer = pointer.nextNode;
+        }
+        pointer.nextNode = null;
+      }
+    }
+  };
+
+  const contains = (val) => {
+    let pointer = head;
+    while (pointer != null) {
+      if (pointer.value == val) {
+        return true;
+      } else {
+        pointer = pointer.nextNode;
+      }
+    }
+    return false;
+  };
+
+  const find = (val) => {
+    let pointer = head;
+    let counter = 0;
+    while (pointer != null) {
+      if (pointer.value == val) {
+        return counter;
+      } else {
+        pointer = pointer.nextNode;
+        counter++;
+      }
+    }
+    return null;
+  };
+
+  const listToString = () => {
+    let pointer = head;
+    let str = ``;
+    while (pointer != null) {
+      str += `( ${pointer.value} ) -> `;
+      pointer = pointer.nextNode;
+    }
+    str += `null`;
+    console.log(str);
   };
 
   const printList = () => {
@@ -52,5 +130,38 @@ const linkedList = (() => {
       console.log(p);
     }
   };
-  return { head, append, prepend, size, printList };
+  return {
+    head,
+    append,
+    prepend,
+    size,
+    getHead,
+    getTail,
+    at,
+    pop,
+    contains,
+    find,
+    listToString,
+    printList,
+  };
 })();
+
+// Tests
+linkedList.append(234);
+linkedList.append(13);
+linkedList.append(48);
+linkedList.printList();
+console.log(linkedList.size());
+linkedList.prepend(69);
+linkedList.printList();
+console.log(linkedList.size());
+console.log(linkedList.getHead());
+console.log(linkedList.getTail());
+linkedList.at(7);
+linkedList.at(2);
+linkedList.pop();
+linkedList.printList();
+console.log(linkedList.contains(17));
+console.log(linkedList.contains(13));
+console.log(linkedList.find(13));
+linkedList.listToString();
